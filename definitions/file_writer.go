640987efc8fc6ed99e3680b4cdc737a9b8f5be84
Chris Dell <cjdell@gmail.com>
2015-07-19 21:33:50 +0100

    writer update

--- definitions/file_writer.go
+++ definitions/file_writer.go
@@ -7,13 +7,11 @@ import (
 type FileWriter struct {
 	matcher1 *regexp.Regexp
 	matcher2 *regexp.Regexp
-	matcher3 *regexp.Regexp
 }
 
 func (def *FileWriter) Init() {
 	def.matcher1, _ = regexp.Compile(`\$_(REQUEST|POST)\[`)
-	def.matcher2, _ = regexp.Compile(`fopen\(\$`)
-	def.matcher3, _ = regexp.Compile(`chmod\(\$`)
+	def.matcher2, _ = regexp.Compile(`fwrite\(\$fp\,\"\\xEF\\xBB\\xBF`)
 }
 
 func (FileWriter) Name() string {
@@ -21,5 +19,5 @@ func (FileWriter) Name() string {
 }
 
 func (def *FileWriter) Check(source string) bool {
-	return def.matcher1.MatchString(source) && def.matcher2.MatchString(source) && def.matcher3.MatchString(source)
+	return def.matcher1.MatchString(source) && def.matcher2.MatchString(source)
 }
